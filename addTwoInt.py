d88e98a86ef4df64b8de657afce5592bcad33971
YoannDL <yoann.lutch@gmail.com>
2021-03-16 14:36:18 +0400

    Le programme python ne prend plus de charactere autres que des chiffres

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,6 +1,10 @@
 def add():
-    a=int(input("Le premier chiffre svp:"))
-    b=int(input("Le deuxieme chiffre svp:"))
+    a=input("Le premier chiffre svp:")
+    while not a.isdigit():
+        a=input("La valeur doit etre numerique veuillez entrer votre chiffre svp: ")
+    b=input("Le deuxieme chiffre svp:")
+    while not b.isdigit():
+        b=input("La valeur doit etre numerique veuillez entrer votre chiffre svp: ")
     c=a+b
     print("La somme des deux chiffres est" ,c)
     
