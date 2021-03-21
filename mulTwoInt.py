8d36b2dd43264e51dd8c50c0f5396f59a0a5edaf
Jameel-Edoo <jameeledoo@outlook.com>
2021-03-21 13:03:36 +0400

    Version 1.0 de la fonction mul

---
+++ mulTwoInt.py
@@ -0,0 +1,13 @@
+def mul():
+    x=input("Le premier chiffre svp:")
+
+    y=input("Le deuxieme chiffre svp:")
+
+    x = int(x)
+    y = int(y)
+    z = x*y
+
+    print("Le produit des deux chiffres est" ,z)
+    
+
+mul()
