e25230568eb28717011d8def21e73428fecf9139
Jameel-Edoo <jameeledoo@outlook.com>
2021-03-21 13:09:10 +0400

    Version 2.0 de la fonction mul

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -1,12 +1,13 @@
 def mul():
     x=input("Le premier chiffre svp:")
-
+    while not x.isdigit():
+        x=input("La valeur doit etre numerique veuillez entrer votre chiffre svp: ")
     y=input("Le deuxieme chiffre svp:")
-
+    while not y.isdigit():
+        y=input("La valeur doit etre numerique veuillez entrer votre chiffre svp: ")
     x = int(x)
     y = int(y)
     z = x*y
-
     print("Le produit des deux chiffres est" ,z)
     
 
